5b0858d5223c852ecdd2922c54ea3304cdc75a9f
hascko <sangare@intechinfo.fr>
2017-01-24 10:19:36 +0100

    Ajouts des domaines

--- view/view_admin_index.php
+++ view/view_admin_index.php
@@ -112,6 +112,14 @@
                                 <div class="panel-body padding-0">
                                     <div class="col-md-12 col-xs-12 col-md-12 padding-0 box-v4-alert">
                                         <h2>Checking Your Server!</h2>
+                                        <table>
+                                        	<tr>
+                                        		<th>Domaines spammers</th>
+                                        		<th>Nombre de spams envoyés</th>
+                                        	</tr>
+                                        	<tr>
+
+                                        	</tr>
                                         <p>Daily Check on Server status, mostly looking at servers with alerts/warnings</p>
                                         <b><span class="icon-clock icons"></span> Today at 15:00</b>
                                     </div>
