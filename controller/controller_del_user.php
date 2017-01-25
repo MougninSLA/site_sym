f1ea39f6fd5d9b8eafd64ad1e031bee146eb0d0f
steephen king <steephen king>
2017-01-25 13:48:01 +0100

    ekngkj

--- controller/controller_del_user.php
+++ controller/controller_del_user.php
@@ -7,6 +7,6 @@ del_user_domain($id_user);
 del_user_blacklist($id_user);
 del_user_whitelist($id_user);
 
-$msg = "<h3 style='color:green'>L'utilisateur choisi a bien été supprimer, ainsi que le(s) domaine(s), blacklist, whitelist qui l'a crée</h3>";
+$msg = "<h3 style='color:green'>L'utilisateur choisi a bien été supprimé, ainsi que le(s) domaine(s), blacklist, whitelist qui l'a crée</h3>";
 include("./controller/controller_admin_users.php");
 ?>
