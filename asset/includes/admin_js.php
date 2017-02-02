b297870eee44bf9a4d00d6c1ade4f25adfe455b9
steephen king <steephen king>
2017-02-02 10:34:59 +0100

    02/02/2017 10:35

--- asset/includes/admin_js.php
+++ asset/includes/admin_js.php
@@ -187,7 +187,7 @@ demo = {
                 tension: 0
             }),
             low: 0,
-            high: <?php echo 0?>, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+            high: <?php echo $max_week;?>, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
             chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
         }
 
